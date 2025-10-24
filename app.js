67ca838b9c78eabf0d0443fad03f0df126a83822
kajanan29 <sivalingamkajanan9@gmail.com>
2025-10-24 23:55:33 +0530

    error handlers

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express=require('express');
 
+const db=require('./data/database');
+
 const path=require('path');
+const addCsrfTokenMiddleware=require('./middleware/csrf-token');
+const errorHandlerMiddleware=require('./middleware/error-handler');
 
 const authRoutes=require('./routes/auth-routes');
 
 const app=express();
 
+const csrf=require('csurf');
+
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended:false}));
+app.use(csrf());
+app.use(addCsrfTokenMiddleware);
 app.use(authRoutes);
+app.use(errorHandlerMiddleware);
 
-app.listen(3000);
+db.connectToDatabase().then(function(){
+   app.listen(3000);
+}).catch(function(error){
+    console.log('falied to connect to the data base');
+    console.log(error);
+}
+);
+
---
+++ models/user-model.js
@@ -0,0 +1,33 @@
+const bcrypt=require('bcryptjs');
+
+const db=require('../data/database');
+
+
+
+class User{
+    constructor(email,password,fullname,street,postal,city){
+        this.email=email;
+        this.password=password;
+        this.fullname=fullname;
+        this.address={
+            street:street,
+            postal:postal,
+            city:city
+        }
+        
+    }
+
+    async signup(){
+        const hashedPassword=await bcrypt.hash(this.password,12);
+
+        await db.getDb().collection('users').insertOne({
+            email:this.email,
+            password:hashedPassword,
+            fullname:this.fullname,
+            address:this.address
+        });
+
+    }
+}
+
+module.exports=User;
